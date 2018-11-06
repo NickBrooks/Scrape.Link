1cc9e83bfce394b24e2680b70381ca53ef557057
Nick Brooks <nbrooks@thirtyseven.com.au>
2018-11-06 15:46:11 -0700

    Cool looking button

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,27 @@
 if (module.hot) {
   module.hot.accept();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  preventUrlSpaces(document.querySelector("#urlInput"));
+  onSubmitUrl(document.querySelector("#urlForm"));
+});
+
+function preventUrlSpaces(urlInput) {
+  urlInput.addEventListener("input", e => {
+    urlInput.value = e.target.value.replace(/\s+/g, "");
+  });
+}
+
+function onSubmitUrl(urlForm) {
+  urlForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const urlInput = document.querySelector("#urlInput");
+    fetch("https://api.scrape.link/scrape", {
+      method: "POST",
+      body: JSON.stringify({ url: urlInput.value })
+    }).then(res => {
+      console.log(res);
+    });
+  });
+}
