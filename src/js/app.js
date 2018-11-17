dbdb2d15b7712fffb72fcdeb4c52b428a7f8b5ba
Nick Brooks <nbrooks@thirtyseven.com.au>
2018-11-16 21:06:01 -0700

    Too many scrapes error message displays

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ if (module.hot) {
 
 // element selectors
 let scraperElement = document.getElementById("scraper");
-let invalidUrlElement = document.getElementById("valid-url");
+let errorMsgElement = document.getElementById("error-message");
 let scrapeButtonElement = document.getElementById("scrape-button");
 let scrapeResultElement = document.getElementById("scrape-result");
 let loaderElement = document.getElementById("loader");
@@ -25,17 +25,31 @@ function preventUrlSpaces(urlInput) {
   });
 }
 
+function fetchScrape(url) {
+  return fetch("https://api.scrape.link/scrape", {
+    method: "POST",
+    body: JSON.stringify({ url })
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(res);
+    }
+  });
+}
+
 function onSubmitUrl(urlForm) {
   urlForm.addEventListener("submit", e => {
     e.preventDefault();
     const urlInput = document.querySelector("#urlInput");
     if (!urlInput.value || !isUrl(urlInput.value)) {
-      invalidUrlElement.classList.remove("hidden");
+      errorMsgElement.innerHTML = "<p>Enter valid URL (incl http/s)!</p>";
+      errorMsgElement.classList.remove("hidden");
       return;
     }
 
     // add loading classes
-    invalidUrlElement.classList.add("hidden");
+    errorMsgElement.classList.add("hidden");
     scraperElement.classList.remove("no-scrape");
     scrapeButtonElement.classList.add("hidden");
     scrapeResultElement.classList.remove("hidden");
@@ -43,13 +57,7 @@ function onSubmitUrl(urlForm) {
     scrapeResultCardElement.innerHTML = "";
     scrapeResultJsonElement.innerHTML = "";
 
-    fetch("https://api.scrape.link/scrape", {
-      method: "POST",
-      body: JSON.stringify({ url: urlInput.value })
-    })
-      .then(res => {
-        return res.json();
-      })
+    fetchScrape(urlInput.value)
       .then(link => {
         // return elements
         scrapeButtonElement.classList.remove("hidden");
@@ -58,6 +66,14 @@ function onSubmitUrl(urlForm) {
         // and generate the results
         scrapeResultCardElement.appendChild(createCard(link));
         scrapeResultJsonElement.appendChild(createJsonResult(link));
+      })
+      .catch(error => {
+        errorMsgElement.innerHTML =
+          "<p>Too many scrapes in this session. Try reloading the browser.</p>";
+        errorMsgElement.classList.remove("hidden");
+        scrapeButtonElement.classList.remove("hidden");
+        scraperElement.classList.add("no-scrape");
+        scrapeResultElement.classList.add("hidden");
       });
   });
 }
